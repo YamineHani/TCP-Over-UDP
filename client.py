db0530173dec2272552936ef9428af9bcc4ee413
Yasmine Hossam ElDin <yasmine.hossam2001@gmail.com>
2024-05-11 14:51:01 +0300

    revert last commit

--- client.py
+++ client.py
@@ -1,86 +1,45 @@
 import socket
-import struct
-import time
-import random
+from TCPOverUDP import send_packet, client_handshake, generate_sequence_number, send_fin
 
-from TCPOverUDP import client_handshake
-
-# Constants for network parameters and window size
+# Define server address and ports
 SERVER_IP = "localhost"
 SERVER_PORT = 12345
-WINDOW_SIZE = 5  # Initial window size
-TIMEOUT = 1  # Timeout for receiving ACK in seconds
-
-# Define global variables for tracking window state
-base_seq_number = 0
-next_seq_number = 0
-max_seq_number = 100
-window = []
-
+seq_number = 0
 # Create UDP socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 
-def send_packet(packet, seq_number):
-    global client_socket
-    global window
-    global base_seq_number
-    global next_seq_number
-
-    # Send packet to server
-    packet_with_seq_num = struct.pack("!B", seq_number) + packet
-    client_socket.sendto(packet_with_seq_num, (SERVER_IP, SERVER_PORT))
-    window.append(seq_number)
-    next_seq_number += 1
-
-
-def receive_ack():
-    global client_socket
-    global window
-    global base_seq_number
-
-    client_socket.settimeout(TIMEOUT)
-    try:
-        ack_packet_with_seq_num, _ = client_socket.recvfrom(1024)
-        ack_seq_number = struct.unpack("!B", ack_packet_with_seq_num[:1])[0]
-        if ack_seq_number in window:
-            window.remove(ack_seq_number)
-            base_seq_number = ack_seq_number + 1
-    except socket.timeout:
-        print("Timeout occurred while waiting for ACK.")
-        pass
-
-
-def main():
-    global base_seq_number
-    global next_seq_number
-
-    # Perform handshake
-    if not client_handshake(client_socket, SERVER_IP, SERVER_PORT):
-        # Exit if handshake fails
-        client_socket.close()
-        exit()
-
-    # Simulated data to be sent
-    data_packets = [
-        b"Hello, World!",
-        b"This is another packet.",
-        b"Yet another packet.",
-        b"POST Yasmine hani",
-        b"GET 100"
-        # Add more data packets as needed
-    ]
-
-    while base_seq_number < max_seq_number:
-        # Send packets within the window size
-        while next_seq_number < base_seq_number + WINDOW_SIZE and next_seq_number < max_seq_number:
-            send_packet(data_packets[next_seq_number], next_seq_number)
-
-        # Receive acknowledgments and slide window
-        receive_ack()
-
-    # Close socket
+# Perform handshake
+if not client_handshake(client_socket, SERVER_IP, SERVER_PORT):
+    # Exit if handshake fails
     client_socket.close()
+    exit()
 
-if __name__ == "__main__":
-    main()
+# Simulated data to be sent
+data_packets = [
+    b"Hello, World!",
+    b"This is another packet.",
+    b"Yet another packet.",
+    b"POST Yasmine hani",
+    b"GET 100"
+    # Add more data packets as needed
+]
+
+# Define packet size
+PACKET_SIZE = 1024
+
+# Loop through each data packet
+for data in data_packets:
+    # Divide data into packets
+    packets = [data[i:i + PACKET_SIZE] for i in range(0, len(data), PACKET_SIZE)]
+
+    # Loop through each packet
+    for packet in packets:
+        # Send packet with sequence number and receive ACK
+        seq_number = send_packet(packet, SERVER_IP, SERVER_PORT, client_socket, seq_number)
+        if seq_number is None:
+            # Exit loop if sending fails
+            break
+
+send_fin(client_socket, SERVER_IP, SERVER_PORT, seq_number)
+
--- server.py
+++ server.py
@@ -1,57 +1,15 @@
 import socket
-import struct
+from TCPOverUDP import server_handshake, receive_packets
 
-# Constants for network parameters and window size
-CLIENT_IP = "localhost"
-CLIENT_PORT = 12345
-
-# Define global variables for tracking window state
+# Define server address and ports
+SERVER_IP = "localhost"
+SERVER_PORT = 12345
 expected_seq_number = 0
-recv_window_size = 5  # Receive window size
-recv_window = []
-
 # Create UDP socket
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_socket.bind((CLIENT_IP, CLIENT_PORT))
+server_socket.bind((SERVER_IP, SERVER_PORT))
 
 
-def receive_packet():
-    global server_socket
-    global expected_seq_number
-    global recv_window
-    global recv_window_size
+if server_handshake(server_socket):
+    receive_packets(server_socket, expected_seq_number)
 
-    packet_with_seq_num, client_address = server_socket.recvfrom(1024)
-    seq_number = struct.unpack("!B", packet_with_seq_num[:1])[0]
-    packet = packet_with_seq_num[1:]
-
-    if seq_number == expected_seq_number:
-        # Packet received in order, deliver to application layer
-        print("Packet received:", packet.decode())
-        expected_seq_number += 1
-
-        # Slide window and deliver in-order packets
-        while expected_seq_number in recv_window:
-            recv_window.remove(expected_seq_number)
-            expected_seq_number += 1
-    elif seq_number < expected_seq_number + recv_window_size:
-        # Packet out of order, buffer in receive window
-        recv_window.append(seq_number)
-        send_ack(expected_seq_number - 1, client_address)  # Acknowledge last in-order packet
-    else:
-        # Packet outside of receive window, discard or ignore
-        pass
-
-
-def send_ack(ack_seq_number, client_address):
-    global server_socket
-    ack_packet = struct.pack("!B", ack_seq_number) + b"ACK"
-    server_socket.sendto(ack_packet, client_address)
-
-
-def main():
-    while True:
-        receive_packet()
-
-if __name__ == "__main__":
-    main()
